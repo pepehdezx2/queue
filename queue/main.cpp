3943a9ce9c21957db219d35c8406619dfbb0219a
José Antonio Hernández <pepehdezx2@gmail.com>
2018-10-30 17:55:03 -0600

    first commit

--- queue/main.cpp
+++ queue/main.cpp
@@ -7,9 +7,46 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <queue>
+#include <stack>
+#include <string>
+using namespace std;
+int main() {
+//    queue<char> palabra;
+//    char letra;
+//    do{
+//        cin >> letra;
+//        palabra.push(letra);
+//    }while(letra!='.');
+//        while(!palabra.empty()){
+//            cout << palabra.front();
+//            palabra.pop();
+//        }
+    
+//    stack<char> pal;
+//    string palabra;
+//    long n;
+//    bool band=true;
+//    cin >> palabra;
+//    n=palabra.length();
+//    for(int i=0; i<n; i++){
+//        pal.push(palabra[i]);
+//    }
+//    for(int i=0; i<n; i++){
+//        if(pal.top()!=palabra[i]){
+//            band=false;
+//        }
+//        else{
+//            pal.pop();
+//        }
+//    }
+//    if (band==true){
+//        cout << "es palindromo" << endl;
+//    }
+//    else{
+//        cout << "no es palindromo" << endl;
+//    }
+    
+    
     return 0;
 }
